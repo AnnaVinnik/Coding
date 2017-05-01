95f52ca37fd5ebe1151acaed1136068ad59e0e3c
AnnaVinnik <vinnik.anna@rambler.ru>
2017-05-01 11:58:17 +0700

    Connected fgetc and transleted to the binary:

--- main.c
+++ main.c
@@ -4,29 +4,36 @@
 
 int main()
 {
-FILE *fc;
-int i, num = 0; 
-char arr[4], a[5] = {};
-//char arrey[2][10];
+FILE *fc, *f2;
+int i, num = 0, j; 
+char arr[4] = {};  //массив с файла
+char a[5][10] = {};  //массив с двоичным кодом чисел
+
 
     fc = fopen("text.txt", "r");
+    f2 = fopen("t2.txt", "a");
+
 
     for (i = 0; i < 3; i++){
        arr[i] = fgetc (fc);
     }
 
-    //for (i = 0; arr[0][i] != 0; i++);
-    //k = i;
+//Заполняем а[0] двоичным кодом числа
+    for (j = 0; j < 3; j++){
+        for (i = 0; i < 8; i++){
+            a[j][i] = ((arr[j]-48)>>i)&1;
+        }
+    }
 
-   /* for (i = 0; i < 8; i++){
-       arr[7-i] = (num >> i)&1;
-    }*/
+for (j = 0; j < 3; j++){
+    for (i = 7; i >= 0; i--)
+        printf("%d ", a[j][i]);
+    printf("\n");
+}
 
 
-   printf("%s ", arr);
-
-printf("\n");
-
+//printf("\n");
     fclose(fc);
+    fclose(f2);
     return 0;
 }
