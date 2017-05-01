435851925201980742b0c9a7b8b4b04adfab23c2
AnnaVinnik <vinnik.anna@rambler.ru>
2017-05-01 12:46:59 +0700

    Counting the number of characters in a first number

--- main.c
+++ main.c
@@ -4,22 +4,26 @@
 
 int main()
 {
-FILE *fc, *f2;
-int i, num = 0, j; 
-char arr[4] = {};  //массив с файла
+FILE *fc;
+int i, num = 0, j, k = 0; 
+char arr[5] = {};  //массив с файла
 char a[5][10] = {};  //массив с двоичным кодом чисел
 
 
     fc = fopen("text.txt", "r");
-    f2 = fopen("t2.txt", "a");
 
-
-    for (i = 0; i < 3; i++){
+    for (i = 0; i < 5; i++){
        arr[i] = fgetc (fc);
     }
+//Подсчет символов в числе
+    for (i = 0; arr[i] != 32; i++){
+         k = k + 1;
+    }
+    
 
+printf ("\n k: %d \n", k);
 //Заполняем а[0] двоичным кодом числа
-    for (j = 0; j < 3; j++){
+    for (j = 0; j < k; j++){
         if (arr[j] > 48 && arr[j] < 58)
             for (i = 0; i < 8; i++){
                 a[j][i] = ((arr[j]-48)>>i)&1;
@@ -30,15 +34,18 @@ char a[5][10] = {};  //массив с двоичным кодом чисел
             }
     }
 
-for (j = 0; j < 3; j++){
-    for (i = 7; i >= 0; i--)
+
+
+//Выводим на экран
+for (j = 0; j < k; j++){
+    for (i = 7; i >= 0; i--){
         printf("%d ", a[j][i]);
+    }
     printf("\n");
 }
 
 
 //printf("\n");
     fclose(fc);
-    fclose(f2);
     return 0;
 }
