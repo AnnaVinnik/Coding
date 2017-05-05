e375aa1666c97f5fd620862d702ce7b24e0b5863
AnnaVinnik <vinnik.anna@rambler.ru>
2017-05-05 21:30:35 +0700

    Fixed binary translation

--- coder.c
+++ coder.c
@@ -1,18 +1,70 @@
+#include "coder.h"
 #include <stdio.h>
 
-int search_x(){
-	char n[] = "0xa88c";
-	for (int i = 0; i < 10; i++){
-		if (n[i] == 'x')
-		return i+1;
-	}
-		return -1;
+void cod_16(char a[5][5], int k){
+int i, j, cod[5][10] = {};    
+    for (j = 0; j < k; j++){
+        cod[0][0] = 1;
+        cod[0][1] = 1;
+        cod[0][2] = 1;
+        cod[0][3] = 0;
+        for ( i = 4; i < 8; i++)
+            cod[0][i] = a[0][i-4];
+        cod[1][0] = 1;
+        cod[1][1] = 0;
+        for (i = 2; i < 6; i++)
+            cod[1][i] = a[1][i-2];
+        for (i = 6; i < 8; i++)
+            cod[1][i] = a[2][i-6];
+        cod[2][0] = 1;
+        cod[2][1] = 0;
+        for (i = 2; i < 4; i++)
+            cod[1][i] = a[2][i];
+        for (i = 4; i < 8; i++)
+            cod[2][i] = a[3][i-4];
+        }
+
+for (i = 0; i < 8; i++)
+    printf ("%d", cod[0][i]);
+printf("\n");
+
+for (i = 0; i < 8; i++)
+    printf ("%d", cod[1][i]);
+printf("\n");
+
+for (i = 0; i < 8; i++)
+    printf ("%d", cod[2][i]);
+printf("\n");
+ 
 }
 
-int main()
-{
-	int i;
-	i = search_x();
-	printf("%d \n", i);
+void cod_7(char a[5][5], int k){
+int i, j, cod[5][10] = {};
+    for (j = 0; j < k; j++){
+        cod[0][0] = 0;
+        for (i = 1; i < 4; i++)
+            cod[0][i] = a[j][i - 1];
+    }
+for (i = 0; i < 4; i++)
+    printf ("%d", cod[0][i]);
+printf("\n");
 
+}
+
+void cod_11(char a[5][5], int k, int num){
+int i, j, z, cod[5][5] = {};
+z = 11 - num;
+         cod[0][0] = 1;
+         cod[0][1] = 1;
+         cod[0][2] = 0;
+         for (i = 0; i < z; i++)
+             cod[0][i + 3] = 0;
+        //for (i = 0; a[i] != '\0'; i++);
+
+         cod[1][0] = 1;
+         cod[1][1] = 0;
+printf("6 %d \n", cod[0][6]);
+for (i = 0; i < 8; i++)
+    printf ("%d", cod[0][i]);
+printf("\n");
 }
--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+#include "coder.h"
 
-//Перевод в двоичную
 
 int main()
 {
 FILE *fc;
-int i, num = 0, j, k = 0, k2 = 0, number = 0, cod[5][10] = {1, 1, 1};
-char arr[5] = {};  //массив с файла
-char a[5][5] = {};  //массив с двоичным кодом чисел
+int i, num = 0, j, k = 0, k2 = 0, number = 0;
+int arr[5] = {};  //массив с файла
+char a[5][22] = {};  //массив с двоичным кодом чисел
 
 
     fc = fopen("text.txt", "r");
@@ -15,74 +15,52 @@ char a[5][5] = {};  //массив с двоичным кодом чисел
     for (i = 0; i < 5; i++){
        arr[i] = fgetc (fc);
     }
+
 //Подсчет символов в числе
     for (i = 0; arr[i] != 32; i++){
          k = k + 1;
     }
-    
-
-printf ("\n k: %d \n", k);
+printf("k: %d \n", k);
+printf ("arr[0]: %d \n", arr[0]);
 //Заполняем а[0] двоичным кодом числа
     for (j = 0; j < k; j++){
-        if (arr[j] > 48 && arr[j] < 58)
+        if (arr[j] > 48 && arr[j] < 58){
             for (i = 0; i < 4; i++){
-                a[j][3 - i] = ((arr[j]-48)>>i)&1;
+             a[0][(j + 1)*4 - i - 1] = ((arr[j]-48)>>i)&1;
             }
-        else if (arr[j] > 96 && arr[j] < 103)
-            for (i = 0; i < 4; i++){
-                a[j][3 - i] = ((arr[j]-87)>>i)&1;
-            }
-    }
+        }
+        else if (arr[j] > 96 && arr[j] < 103){
+                a[j][j*4] = ((arr[j]-87)>>i)&1;
+        }
+   }
 
-    for (i = 0; a[0][i] == 0 ; i++);
+
+printf("a: %d ", a[0][0]);
+printf("%d ", a[0][1]);
+printf("%d ", a[0][2]);
+printf("%d ", a[0][3]);
+printf("%d ", a[0][4]);
+printf("%d ", a[0][5]);
+printf("%d ", a[0][6]);
+printf("%d \n", a[0][7]);
+
+/*    for (i = 0; a[0][i] == 0 ; i++){
+    printf("n: %d \n", i);
     k2 = i;
+    }
     number = k*4 - k2;//Количество значащих цифр
-printf("%d \n", number);
+printf("number: %d k2: %d \n", number, k2);*/
 
-    if (number == 16){
-    for (j = 0; j < k; j++){
-        cod[0][0] = 1;
-        cod[0][1] = 1;
-        cod[0][2] = 1;
-        cod[0][3] = 0;
-        for ( i = 4; i < 8; i++)
-            cod[0][i] = a[0][i-4];
-        cod[1][0] = 1;
-        cod[1][1] = 0;
-        for (i = 2; i < 6; i++)
-            cod[1][i] = a[1][i-2];
-        for (i = 6; i < 8; i++)
-            cod[1][i] = a[2][i-6];
-        cod[2][0] = 1;
-        cod[2][1] = 0;
-        for (i = 2; i < 4; i++)
-            cod[1][i] = a[2][i];
-        for (i = 4; i < 8; i++)
-            cod[2][i] = a[3][i-4];
-
+  /*  if (k <= 2){
+        cod_2(a, k);
     }
-}
-    
-
-//Выводим на экран
-for (j = 0; j < k; j++){
-    for (i = 0; i < 4; i++){
-        printf("%d ", a[j][i]);
+    else if (number <= 7){
+        cod_7(a, k);
     }
-    printf("\n");
-}
+    else if (number > 7){
+        cod_11(a, k, number);
+    }*/
 
-for (i = 0; i < 8; i++)
-    printf ("%d", cod[0][i]);
-printf("\n");
-
-for (i = 0; i < 8; i++)
-    printf ("%d", cod[1][i]);
-printf("\n");
-
-for (i = 0; i < 8; i++)
-    printf ("%d", cod[2][i]);
-printf("\n");
 
 
     fclose(fc);
