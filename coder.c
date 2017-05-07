9b55c18d68018ebb54e2c5cb9a64c478ceec875d
AnnaVinnik <vinnik.anna@rambler.ru>
2017-05-07 14:51:05 +0700

    Add coding

--- coder.c
+++ coder.c
@@ -1,80 +1,12 @@
 #include "coder.h"
 #include <stdio.h>
-#define SIZE_ARRAY 7
+#define SIZE_ARRAY 55
 
 //FILE *fput;
 
 //fput = fopen("t2.txt", "a");
 
-void cod_16(char a[5][5], int k){
-int i, j, cod[5][10] = {};    
-    for (j = 0; j < k; j++){
-        cod[0][0] = 1;
-        cod[0][1] = 1;
-        cod[0][2] = 1;
-        cod[0][3] = 0;
-        for ( i = 4; i < 8; i++)
-            cod[0][i] = a[0][i-4];
-        cod[1][0] = 1;
-        cod[1][1] = 0;
-        for (i = 2; i < 6; i++)
-            cod[1][i] = a[1][i-2];
-        for (i = 6; i < 8; i++)
-            cod[1][i] = a[2][i-6];
-        cod[2][0] = 1;
-        cod[2][1] = 0;
-        for (i = 2; i < 4; i++)
-            cod[1][i] = a[2][i];
-        for (i = 4; i < 8; i++)
-            cod[2][i] = a[3][i-4];
-        }
-
-for (i = 0; i < 8; i++)
-    printf ("%d", cod[0][i]);
-printf("\n");
-
-for (i = 0; i < 8; i++)
-    printf ("%d", cod[1][i]);
-printf("\n");
-
-for (i = 0; i < 8; i++)
-    printf ("%d", cod[2][i]);
-printf("\n");
- 
-}
-
-void cod_7(char a[5][5], int k){
-int i, j, cod[5][10] = {};
-    for (j = 0; j < k; j++){
-        cod[0][0] = 0;
-        for (i = 1; i < 4; i++)
-            cod[0][i] = a[j][i - 1];
-    }
-for (i = 0; i < 4; i++)
-    printf ("%d", cod[0][i]);
-printf("\n");
-
-}
-
-void cod_11(char a[5][4], int k, int num){
-int i, j, z, cod[5][5] = {};
-z = 11 - num;
-         cod[0][0] = 1;
-         cod[0][1] = 1;
-         cod[0][2] = 0;
-         for (i = 0; i < z; i++)
-             cod[0][i + 3] = 0;
-        //for (i = 0; a[i] != '\0'; i++);
-
-         cod[1][0] = 1;
-         cod[1][1] = 0;
-printf("6 %d \n", cod[0][6]);
-for (i = 0; i < 8; i++)
-    printf ("%d", cod[0][i]);
-printf("\n");
-}
-
-void ShiftTo(char a[1][SIZE_ARRAY], int to, int since)
+void ShiftTo(int a[5][SIZE_ARRAY], int to, int since)
 {
 int i, j;
      for (j = 0; j < to; j++){
@@ -85,12 +17,11 @@ int i, j;
 
 }
 
-void Insert(char a[1][SIZE_ARRAY], int since, int what, int quantity)
+void Insert(int a[5][SIZE_ARRAY], int since, int what, int quantity)
 {
 int i, del = 1;
-    for (i = since; i < (since + quantity); i++){
-        a[0][(since + quantity) - i - 1] = what % 10;
-printf("del %d\t what %d \n", del, what);
+    for (i = (since + quantity); i > since; i--){
+        a[0][i - 1] = what % 10;
         what = what / 10;
 
     }
--- main.c
+++ main.c
@@ -5,15 +5,16 @@
 int main()
 {
 FILE *fc, *fp;
-int i, num = 0, j, k = 0, k2 = 0, number = 0;
+int i, qtZero = 0, j, k = 0, k2 = 0, number = 0;
 int arr[5] = {};  //массив с файла
-int a[5][22] = {};  //массив с двоичным кодом чисел
+int a[5][55] = {};  //массив с двоичным кодом чисел
+char prob = {' '};
 
 
     fc = fopen("text.txt", "r");
     fp = fopen("t2.txt", "a");
 
-    for (i = 0; i < 8; i++){
+    for (i = 0; i < 10; i++){
        arr[i] = fgetc (fc);
     }
 
@@ -21,9 +22,6 @@ int a[5][22] = {};  //массив с двоичным кодом чисел
     for (i = 0; arr[i] != 32; i++){
          k = k + 1;
     }
-printf("k:%d\n", k);
-for (i = 0; i < k; i++)
-    printf("%d\n", arr[i]);
 
 
 //Заполняем а[0] двоичным кодом числа
@@ -40,38 +38,70 @@ for (i = 0; i < k; i++)
         }
     }
 
+printf("k:%d\n", k);
 
-   /* for (i = 0; i < k; i++){
-        printf("%d", a[0][i]);
-    }*/
+//Подсчет первых нулей 
+   for (i = 0; a[0][i] == 0; i++){
+      qtZero = qtZero + 1;
+   }
 
-    //if (k < 3){
-       // for (i = 0; i < 8; i++){
 
-//a[0][0] = 3;            //fprintf(fput, "%s", a[0][0]);
-for (i = 0; i < k*4; i++){
-fprintf(fp, "%d", a[0][i]);
-}
-char prob = {' '};
-fputc(prob, fp);
-        //}
-    //}
-  /*  for (i = 0; a[0][i] == 0 ; i++){
-    printf("n: %d \n", i);
-    k2 = i;
+for (i = 0; i < 32; i++)
+    fprintf(fp, "%d", a[0][i]);
+    fputc(prob, fp);
+
+    if (k == 1){
+        ShiftTo(a, 4, 0);
+        Insert(a, 0, 0000, 4);
+        for (i = 0; i < 8; i++){
+            fprintf(fp, "%d", a[0][i]);
+        }
     }
-    number = k*4 - k2;//Количество значащих цифр
-printf("number: %d k2: %d \n", number, k2);*/
+    
+    if (k == 2){
+        for (i = 0; i < 8; i++){
+            fprintf(fp, "%d", a[0][i]);
+        }
+    }
+    
+    if (k == 3){
+        ShiftTo(a, 3, 0);
+        Insert(a, 0, 110, 3);
+        ShiftTo(a, 2, 8);
+        Insert(a, 8, 10, 2);
+        for (i = 0; i < 16; i++){
+            fprintf(fp, "%d", a[0][i]);
+        }
+        fputc(prob, fp);
+     }
 
-  /*  if (k <= 2){
-        cod_2(a, k);
+    if (k == 4){
+        ShiftTo(a, 4, 0);
+        Insert(a, 0, 1110, 4);
+        ShiftTo(a, 2, 8);
+        Insert(a, 8, 10, 2);
+        ShiftTo(a, 2, 16);
+        Insert(a, 16, 10, 2);
+        for (i = 0; i < 24; i ++){
+            fprintf(fp, "%d", a[0][i]);
+        }
+        fputc(prob, fp);
     }
-    else if (number <= 7){
-        cod_7(a, k);
-    }
-    else if (number > 7){
-        cod_11(a, k, number);
-    }*/
+
+    if (k == 5){
+        ShiftTo(a, 5, 0);
+        Insert(a, 0, 11110, 5);
+        ShiftTo(a, 2, 8);
+        Insert(a, 8, 10, 2);
+        ShiftTo(a, 2, 16);
+        Insert(a, 16, 10, 2);
+        ShiftTo(a, 2, 24);
+        Insert(a, 24, 10, 2);
+        for (i = 0; i < 32; i++){
+            fprintf(fp, "%d", a[0][i]);
+        }
+        fputc(prob, fp);
+    }  
 
 
 
