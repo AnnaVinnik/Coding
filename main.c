dce64e4d8b6c42c8fe74fa75cd138205536d9734
AnnaVinnik <vinnik.anna@rambler.ru>
2017-05-12 20:02:07 +0700

    Changed main

--- main.c
+++ main.c
@@ -25,95 +25,12 @@ char prob = {' '};
 char coder[6] = "coder", decoder[8] = "decoder";
 
  
-    if (argv[1][0] == coder[0]){
-        fd = fopen(argv[2], "r");
-        fc = fopen(argv[3], "a");
-
-        for (i = 0; i < 10; i++){
-            arr[i] = fgetc (fd);
-        }
-
-//Подсчет символов в числе
-        for (i = 0; arr[i] != 32; i++){
-            k = k + 1;
-        }
-
-
-//Заполняем а[0] двоичным кодом числа
-        for (j = 0; j < k; j++){
-            if (arr[j] > 48 && arr[j] < 58){
-                for (i = 0; i < 4; i++){
-                a[0][(j + 1)*4 - i - 1] = ((arr[j]-48)>>i)&1;
-                }
-            }
-            else if (arr[j] > 96 && arr[j] < 103){
-                for (i = 0; i < 4; i++){
-                    a[0][(j + 1)*4 - i - 1] = ((arr[j]-87)>>i)&1;
-                }
-            }
-        }
-
-
-//Подсчет первых нулей 
-        for (i = 0; a[0][i] == 0; i++){
-           qtZero = qtZero + 1;
-        }
-
-        significantNumbers = k*4 - qtZero;
-
-        if (k == 1){
-            ShiftTo(a, 4, 0);
-            Insert(a, 0, 0000, 4);
-            for (i = 0; i < 8; i++){
-                fprintf(fc, "%d", a[0][i]);
-            }
-        }
+    //if (argv[1][0] == coder[0]){
     
-        if (k == 2){
-            ShiftTo(a, 3, 0);
-            Insert(a, 0, 110, 3);
-            ShiftTo(a, 2, 8);
-            Insert(a, 8, 10, 2);
-            for (i = 0; i < 16; i++){
-                fprintf(fc, "%d", a[0][i]);
-            }
-            fputc(prob, fc);
-         }
+   // }
 
-        if (k == 4 || k == 3){
-            ShiftTo(a, 4, 0);
-            Insert(a, 0, 1110, 4);
-            ShiftTo(a, 2, 8);
-            Insert(a, 8, 10, 2);
-            ShiftTo(a, 2, 16);
-            Insert(a, 16, 10, 2);
-            for (i = 0; i < 24; i ++){
-                fprintf(fc, "%d", a[0][i]);
-        }
-            fputc(prob, fc);
-        }
-
-        if (k == 5){
-            ShiftTo(a, 5, 0);
-            Insert(a, 0, 11110, 5);
-            ShiftTo(a, 2, 8);
-            Insert(a, 8, 10, 2);
-            ShiftTo(a, 2, 16);
-            Insert(a, 16, 10, 2);
-            ShiftTo(a, 2, 24);
-            Insert(a, 24, 10, 2);
-            for (i = 0; i < 32; i++){
-                fprintf(fc, "%d", a[0][i]);
-            }
-            fputc(prob, fc);
-        }  
-
-        fclose(fc);
-        fclose(fd);
-
-    }
 //Decoder
-    else if (argv[1][0] == decoder[0] && argv[1][1] == decoder[1]){
+    if (argv[1][0] == decoder[0] && argv[1][1] == decoder[1]){
 
         fd = fopen(argv[3], "a");
         fc = fopen(argv[2], "r");
